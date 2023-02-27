5a310bbfb9d7bea153950aef77573eed7d89aa19
Aleks Lemming <aleks.lemming@tktk.ee>
2023-02-27 18:39:12 +0200

    add week 4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rnd = new Random();
+
+int myRandiNum;
+
+int randomSum = 0;
+
+for(int i = 0; i  < 3; i++)
+{
+
+int myRandimNum = rnd.Next(0, 11);  //0 - 10 
+   
+    randomSum += myRandimNum;
+
+Console.WriteLine($"My random number is: {myRandimNum}");
+
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//FOR/LOOP
+
+//DRY-principle - Do not Repeat Yourself
+
+for(int i = 0; i < 5; i++) // i++ = i + 1
+{
+
+Console.WriteLine("We dont need no education");
+
+
+}
+
+Console.WriteLine("Have a nice day.");
